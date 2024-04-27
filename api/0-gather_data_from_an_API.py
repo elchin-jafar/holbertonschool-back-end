6f4767196bb001c28f353254413fec2917897935
Elchin <elcinceferli@gmail.com>
2024-04-27 18:08:53 +0400

    some bullshit for pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 
+
 def main():
     BASE_URL = "https://jsonplaceholder.typicode.com/"
     r_employee = requests.get("{}users/{}".format(BASE_URL, sys.argv[1]))
@@ -10,15 +11,17 @@ def main():
 
     r_todo = requests.get("{}todos".format(BASE_URL))
 
-
-    result = list(filter(lambda x: x['userId'] == int(sys.argv[1]), r_todo.json()))
+    result = list(filter(lambda x: x['userId'] == int(sys.argv[1]),
+                         r_todo.json()))
     TOTAL_NUMBER_OF_TASKS = len(result)
 
-    done = list(filter(lambda x: x["completed"] == True, result))
+    done = list(filter(lambda x: x["completed"] is True, result))
     NUMBER_OF_DONE_TASKS = len(done)
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    print("Employee {} is done with tasks({}/{}):".
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for todo in done:
         print(todo['title'])
+
 
 if __name__ == "__main__":
     main()
